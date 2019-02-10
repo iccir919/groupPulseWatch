596bf8d65bd0f904f4fb77cce0d90293a998c281
Neil Ricci <iccir919@gmail.com>
2019-02-09 19:14:31 -0800

    Send emails every on every 5 second per minute

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 var schedule = require('node-schedule');
 var http = require('http');
- 
-var j = schedule.scheduleJob('5 * * * * *', function(){
-  console.log('The answer to life, the universe, and everything!');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'grouppulsewatch@gmail.com',
+        pass: 'h3artRat3!'
+    }
 });
 
+var mailOptions = {
+    from: 'grouppulsewatch@gmail.com',
+    to: 'grouppulsewatch@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+ 
+var j = schedule.scheduleJob('5 * * * * *', function(){
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+});
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
