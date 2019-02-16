a088ce108ceee5f0ed5cffbfb409f16970fbc51d
Neil Ricci <iccir919@gmail.com>
2019-02-16 14:27:44 -0800

    Add mailer to scheduled job

--- scheduled-job.js
+++ scheduled-job.js
@@ -1,4 +1,25 @@
-function sayHello() {
-    console.log('Hello');
-}
-sayHello();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'grouppulsewatch@gmail.com',
+        pass: process.env['GMAIL_PASSWORD']
+    }
+});
+
+const mailOptions = {
+from: 'grouppulsewatch@gmail.com',
+to: 'grouppulsewatch@gmail.com',
+subject: 'Daily Resting Heart Rate Email',
+text: 'This email is a test'
+};
+
+
+transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+        console.log(err)
+    else
+        console.log(info);
+})
